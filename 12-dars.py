8f0f807464a02a417bebfdb50c27bd11e4b82e03
Ixt1yor <ixtiyorboshmonov4@gmail.com>
2023-08-02 20:50:17 +0500

    1-faylim qo'shildi
    12-dars kodi

---
+++ 12-dars.py
@@ -0,0 +1,13 @@
+# a,b=map(int,input("2 ta son kiriting>>>").split())
+# print(f"{a}>{b}") if a>b else print(f"{a}<{b}")
+# foydalanuvchilar=['adham','ixtiyor','kamron','odil','ali']
+# yangi_login=input("Yangi login kiriting>>>")
+# if yangi_login in foydalanuvchilar:
+#     print("Login band, yangi login tanlang!")
+# else:
+#     print("Xush kelibsiz, foydalanuvchi!")
+n=int(input("Istalgan son kiriting>>>"))
+for s in range(2,11):
+    if n%s==0:
+        print(f"{n} soni {s} ga qoldiqsiz bo'linadi")
+        
